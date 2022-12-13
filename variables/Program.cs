39cd0f0f36ef90f18d6e1ac3cd293a28f08947b1
saidatla201 <saidatla201@gmail.com>
2022-12-13 08:50:47 +0530

    [saidatla201] variables

---
+++ variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace variables
+{
+    internal class Program
+    {
+        public static void Main()
+        // variables 
+        {
+            int age = 24;
+            string firstname = "sai";
+            string lastname = "datla";
+            string fullname = firstname + lastname;
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your full name is " + fullname);
+            Console.ReadLine();
+        }
+
+        }
+    }
